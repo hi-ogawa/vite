e7e0620c746a693b38fee7a0f879403a33454920
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-27 11:39:15 +0900

    test(v6): test `pluginContainer.resolveId` and `mainFields`

--- packages/vite/src/node/__tests__/dev.spec.ts
+++ packages/vite/src/node/__tests__/dev.spec.ts
@@ -1,5 +1,6 @@
+import { fileURLToPath } from 'node:url'
 import { describe, expect, test } from 'vitest'
-import { resolveConfig } from '..'
+import { createServer, resolveConfig } from '..'
 
 describe('resolveBuildEnvironmentOptions in dev', () => {
   test('build.rollupOptions should not have input in lib', async () => {
@@ -15,5 +16,104 @@ describe('resolveBuildEnvironmentOptions in dev', () => {
     )
 
     expect(config.build.rollupOptions).not.toHaveProperty('input')
+  })
+})
+
+describe('resolveId', () => {
+  test('mainFields default', async () => {
+    const server = await createServer({
+      configFile: false,
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      logLevel: 'silent',
+      optimizeDeps: {
+        noDiscovery: true,
+      },
+      environments: {
+        custom: {},
+      },
+    })
+    expect(
+      (
+        await server.pluginContainer.resolveId(
+          '@vitejs/dep-main-fields',
+          undefined,
+        )
+      )?.id,
+    ).toContain('module.mjs')
+    expect(
+      (
+        await server.environments.custom.pluginContainer.resolveId(
+          '@vitejs/dep-main-fields',
+          undefined,
+        )
+      )?.id,
+    ).toContain('module.mjs')
+  })
+
+  test('mainFields empty environment', async () => {
+    const server = await createServer({
+      configFile: false,
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      logLevel: 'silent',
+      optimizeDeps: {
+        noDiscovery: true,
+      },
+      environments: {
+        custom: {
+          resolve: {
+            mainFields: [],
+          },
+        },
+      },
+    })
+    expect(
+      (
+        await server.pluginContainer.resolveId(
+          '@vitejs/dep-main-fields',
+          undefined,
+        )
+      )?.id,
+    ).toContain('module.mjs')
+    expect(
+      (
+        await server.environments.custom.pluginContainer.resolveId(
+          '@vitejs/dep-main-fields',
+          undefined,
+        )
+      )?.id,
+    ).toContain('main.cjs')
+  })
+
+  test('mainFields empty top level', async () => {
+    const server = await createServer({
+      configFile: false,
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      logLevel: 'silent',
+      optimizeDeps: {
+        noDiscovery: true,
+      },
+      resolve: {
+        mainFields: [],
+      },
+      environments: {
+        custom: {},
+      },
+    })
+    expect(
+      (
+        await server.pluginContainer.resolveId(
+          '@vitejs/dep-main-fields',
+          undefined,
+        )
+      )?.id,
+    ).toContain('main.cjs')
+    expect(
+      (
+        await server.environments.custom.pluginContainer.resolveId(
+          '@vitejs/dep-main-fields',
+          undefined,
+        )
+      )?.id,
+    ).toContain('main.cjs')
   })
 })
