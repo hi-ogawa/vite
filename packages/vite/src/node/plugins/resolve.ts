vitejs/rolldown-vite

feat: use rolldown for build
Co-authored-by: underfin <likui.underfin@gmail.com>

packages/vite/src/node/plugins/resolve.ts
@@ -1,7 +1,7 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import colors from 'picocolors'
-import type { PartialResolvedId } from 'rollup'
+import type { PartialResolvedId } from 'rolldown'
 import { exports, imports } from 'resolve.exports'
 import { hasESMSyntax } from 'mlly'
 import type { Plugin } from '../plugin'
@@ -190,7 +190,13 @@ export function resolvePlugin(
    */
   environmentsOptions?: Record<string, ResolvedEnvironmentOptions>,
 ): Plugin {
-  const { root, isProduction, asSrc, preferRelative = false } = resolveOptions
+  const {
+    root,
+    isProduction,
+    isBuild,
+    asSrc,
+    preferRelative = false,
+  } = resolveOptions
 
   // In unix systems, absolute paths inside root first needs to be checked as an
   // absolute URL (/root/root/path-to-file) resulting in failed checks before falling
@@ -225,9 +231,7 @@ export function resolvePlugin(
         return id
       }
 
-      // this is passed by @rollup/plugin-commonjs
-      const isRequire: boolean =
-        resolveOpts?.custom?.['node-resolve']?.isRequire ?? false
+      const isRequire: boolean = resolveOpts.kind === 'require-call'
 
       const environmentName = this.environment.name ?? (ssr ? 'ssr' : 'client')
       const currentEnvironmentOptions =
@@ -504,16 +508,41 @@ export function resolvePlugin(
 
     load(id) {
       if (id.startsWith(browserExternalId)) {
-        if (isProduction) {
-          return `export default {}`
+        if (isBuild) {
+          if (isProduction) {
+            // rolldown treats missing export as an error, and will break build.
+            // So use cjs to avoid it.
+            return `module.exports = {}`
+          } else {
+            id = id.slice(browserExternalId.length + 1)
+            // rolldown uses esbuild interop helper, so copy the proxy module from https://github.com/vitejs/vite/blob/main/packages/vite/src/node/optimizer/esbuildDepPlugin.ts#L259
+            return `\
+module.exports = Object.create(new Proxy({}, {
+  get(_, key) {
+    if (
+      key !== '__esModule' &&
+      key !== '__proto__' &&
+      key !== 'constructor' &&
+      key !== 'splice'
+    ) {
+      throw new Error(\`Module "${id}" has been externalized for browser compatibility. Cannot access "${id}.\${key}" in client code.  See https://vitejs.dev/guide/troubleshooting.html#module-externalized-for-browser-compatibility for more details.\`)
+    }
+  }
+  }))`
+          }
         } else {
-          id = id.slice(browserExternalId.length + 1)
-          return `\
+          // in dev, needs to return esm
+          if (isProduction) {
+            return `export default {}`
+          } else {
+            id = id.slice(browserExternalId.length + 1)
+            return `\
 export default new Proxy({}, {
   get(_, key) {
     throw new Error(\`Module "${id}" has been externalized for browser compatibility. Cannot access "${id}.\${key}" in client code.  See https://vitejs.dev/guide/troubleshooting.html#module-externalized-for-browser-compatibility for more details.\`)
   }
 })`
+          }
         }
       }
       if (id.startsWith(optionalPeerDepId)) {

hi-ogawa/vite

packages/vite/src/node/plugins/resolve.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import colors from 'picocolors'
-import type { PartialResolvedId } from 'rollup'
+import type { PartialResolvedId } from 'rolldown'
 import { exports, imports } from 'resolve.exports'
 import { hasESMSyntax } from 'mlly'
 import type { Plugin } from '../plugin'
@@ -189,7 +189,13 @@ export function resolvePlugin(
    */
   environmentsOptions?: Record<string, ResolvedEnvironmentOptions>,
 ): Plugin {
-  const { root, isProduction, asSrc, preferRelative = false } = resolveOptions
+  const {
+    root,
+    isProduction,
+    isBuild,
+    asSrc,
+    preferRelative = false,
+  } = resolveOptions
 
   // In unix systems, absolute paths inside root first needs to be checked as an
   // absolute URL (/root/root/path-to-file) resulting in failed checks before falling
@@ -224,9 +230,7 @@ export function resolvePlugin(
         return id
       }
 
-      // this is passed by @rollup/plugin-commonjs
-      const isRequire: boolean =
-        resolveOpts?.custom?.['node-resolve']?.isRequire ?? false
+      const isRequire: boolean = resolveOpts.kind === 'require-call'
 
       const environmentName = this.environment.name ?? (ssr ? 'ssr' : 'client')
       const currentEnvironmentOptions =
@@ -487,16 +491,41 @@ export function resolvePlugin(
 
     load(id) {
       if (id.startsWith(browserExternalId)) {
-        if (isProduction) {
-          return `export default {}`
+        if (isBuild) {
+          if (isProduction) {
+            // rolldown treats missing export as an error, and will break build.
+            // So use cjs to avoid it.
+            return `module.exports = {}`
+          } else {
+            id = id.slice(browserExternalId.length + 1)
+            // rolldown uses esbuild interop helper, so copy the proxy module from https://github.com/vitejs/vite/blob/main/packages/vite/src/node/optimizer/esbuildDepPlugin.ts#L259
+            return `\
+module.exports = Object.create(new Proxy({}, {
+  get(_, key) {
+    if (
+      key !== '__esModule' &&
+      key !== '__proto__' &&
+      key !== 'constructor' &&
+      key !== 'splice'
+    ) {
+      throw new Error(\`Module "${id}" has been externalized for browser compatibility. Cannot access "${id}.\${key}" in client code.  See https://vite.dev/guide/troubleshooting.html#module-externalized-for-browser-compatibility for more details.\`)
+    }
+  }
+  }))`
+          }
         } else {
-          id = id.slice(browserExternalId.length + 1)
-          return `\
+          // in dev, needs to return esm
+          if (isProduction) {
+            return `export default {}`
+          } else {
+            id = id.slice(browserExternalId.length + 1)
+            return `\
 export default new Proxy({}, {
   get(_, key) {
     throw new Error(\`Module "${id}" has been externalized for browser compatibility. Cannot access "${id}.\${key}" in client code.  See https://vite.dev/guide/troubleshooting.html#module-externalized-for-browser-compatibility for more details.\`)
   }
 })`
+          }
         }
       }
       if (id.startsWith(optionalPeerDepId)) {