75bca1771016fde7015d0f984a0efc0b1fd340bd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-11-11 19:11:23 +0900

    chore: cleanup

--- playground/rolldown-dev-react/src/main.tsx
+++ playground/rolldown-dev-react/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOMClient from 'react-dom/client'
-import { App } from './app.tsx'
+import { App } from './app'
 
 ReactDOMClient.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
