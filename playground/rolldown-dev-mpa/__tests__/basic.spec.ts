335d8537cbecdd444a3d55dfd066a678343bd215
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-12-09 17:49:22 +0900

    test: test shared chunk

--- playground/rolldown-dev-mpa/__tests__/basic.spec.ts
+++ playground/rolldown-dev-mpa/__tests__/basic.spec.ts
@@ -2,10 +2,13 @@ import { test } from 'vitest'
 import { page } from '../../test-utils'
 
 test('basic', async () => {
+  page.setDefaultTimeout(1000)
   await page.getByRole('heading', { name: 'Home' }).click()
   await page.getByText('Rendered by /index.js').click()
+  await page.getByText('shared: [ok]').click()
   await page.getByRole('link', { name: 'About' }).click()
   await page.waitForURL(/\/about/)
   await page.getByRole('heading', { name: 'About' }).click()
   await page.getByText('Rendered by /about/index.js').click()
+  await page.getByText('shared: [ok]').click()
 })
